4700f7f18a76a6b4544648eac2045801bc678acd
MikHut <mhutchinson@sagarobotics.com>
2021-01-28 16:53:39 +0000

    added publishers, made nice parse, tidied, sorted pitch coord system for ros

--- scripts/gsof_driver.py
+++ scripts/gsof_driver.py
@@ -9,10 +9,35 @@ It  has been adapted from https://kb.unavco.org/kb/article/trimble-netr9-receive
 
 from struct import unpack
 from trimble_gnss_driver.conversions import *
+from trimble_gnss_driver.parser import parse_maps
 import socket
+import sys
 import math
-import argparse
 import rospy
+
+from geometry_msgs.msg import PoseWithCovarianceStamped # For orientation
+from sensor_msgs.msg import NavSatFix, NavSatStatus # For lat lon h
+from tf.transformations import quaternion_from_euler
+
+
+"""
+GSOF messages from https://www.trimble.com/OEM_ReceiverHelp/#GSOFmessages_Overview.html?TocPath=Output%2520Messages%257CGSOF%2520Messages%257COverview%257C_____0
+"""
+# Records we most care about
+ATTITUDE = 27          # Attitude information with errors
+LAT_LON_H = 2          # Position lat lon h
+POSITION_SIGMA = 12    # Errors in position
+INS_FULL_NAV = 49      # INS fused full nav info pose, attittude etc
+INS_RMS = 50           # RMS errors from reported fused position
+
+# Others
+VELOCITY = 8
+SERIAL_NUM = 15
+GPS_TIME = 1
+UTC_TIME = 16
+ECEF_POS = 3
+BASE_POSITION = 41
+
 
 class GsofDriver(object):
     """ A class to parse GSOF messages from a TCP stream. """
@@ -24,6 +49,14 @@ class GsofDriver(object):
         port = rospy.get_param('~rtk_port', 21098)
         ip = rospy.get_param('~rtk_ip','192.168.0.50')
 
+        self.gps_main_frame_id = rospy.get_param('~gps_main_frame_id', 'gps_link')
+        self.base_frame = rospy.get_param("base_frame", "base_link")
+
+        self.fix_pub = rospy.Publisher('fix', NavSatFix, queue_size=1)
+        # For attitude, use Pose type to avoid confusion that data might be from
+        # IMU only but at the same time keep compatible with robot_localization
+        self.attitude_pub = rospy.Publisher('attitude', PoseWithCovarianceStamped, queue_size=1)
+
         self.client = self.setup_connection(ip , port)
 
         self.msg_dict = {}
@@ -31,43 +64,134 @@ class GsofDriver(object):
         self.checksum = None
         self.rec_dict = {}
 
-        # Get reference coordinates from first epoch if not provided by user
-        self.get_message_header()
-        self.get_records()
-        # ref_x, ref_y, ref_z = self.rec_dict['X_POS'], self.rec_dict['Y_POS'], self.rec_dict['Z_POS']
-        # print "# Reference not specified, using first epoch: X =%.4f Y=%.4f Z=%.4f" % (ref_x, ref_y, ref_z)
-        print ""
-        print "WN - GPS WEEK"
-        print "SOW - GPS time"
-        print "FLAG1 FLAG2" 
-        print "SATS - # SVS"
-        print "X[m] Y[m] Z[m]"
-        print "eX[m] eY[m] eZ[m] - Position VCV Position RMS"
-        print ""
-        print "# WN  SOW     FLAG1 FLAG2 SATS  X[m]          Y[m]            Z[m]       eX[m]   eY[m]     eZ[m]   E[m]   N[m]   U[m]"
-        while 1:
+        self.ins_rms_ts = rospy.Time()
+        self.ins_rms_timeout = 0.5
+
+        self.gps_qualities = {
+            # Unknown
+            -1: [
+                NavSatStatus.STATUS_NO_FIX,
+                NavSatFix.COVARIANCE_TYPE_UNKNOWN
+            ],
+            # Invalid
+            0: [
+                NavSatStatus.STATUS_NO_FIX,
+                NavSatFix.COVARIANCE_TYPE_UNKNOWN
+            ],
+            # SPS
+            1: [
+                NavSatStatus.STATUS_FIX,
+                NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+            ],
+            # DGPS
+            2: [
+                NavSatStatus.STATUS_SBAS_FIX,
+                NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+            ],
+            # PPS
+            3: [
+                NavSatStatus.STATUS_SBAS_FIX,
+                NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+            ],
+            # RTK Fix
+            4: [
+                NavSatStatus.STATUS_GBAS_FIX,
+                NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+            ],
+            # RTK Float
+            5: [
+                NavSatStatus.STATUS_GBAS_FIX,
+                NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+            ],
+            # Estimated (dead reckoning) mode
+            6: [
+                NavSatStatus.STATUS_NO_FIX,
+                NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+            ],
+            # Manual input mode
+            7: [
+                NavSatStatus.STATUS_SBAS_FIX,
+                NavSatFix.COVARIANCE_TYPE_APPROXIMATED
+            ]
+        }
+
+        while not rospy.is_shutdown():
             # READ GSOF STREAM
+            self.records = []
             self.get_message_header()
             self.get_records()
-            # # PRINT GSOF STREAM
-            # x = self.rec_dict['X_POS']
-            # y = self.rec_dict['Y_POS']
-            # z = self.rec_dict['Z_POS']
-            # output = "%04d %.3f %3d %3d %2d %14.4f %14.4f %14.4f %7.4f %7.4f %7.4f" % (
-            #     self.rec_dict['GPS_WEEK'],
-            #     self.rec_dict['GPS_TIME']/1000.0,
-            #     self.rec_dict['FLAG_1'],
-            #     self.rec_dict['FLAG_2'],
-            #     self.rec_dict['SVN_NUM'],
-            #     self.rec_dict['X_POS'],
-            #     self.rec_dict['Y_POS'],
-            #     self.rec_dict['Z_POS'],
-            #     math.sqrt(self.rec_dict['VCV_XX']),
-            #     math.sqrt(self.rec_dict['VCV_YY']),
-            #     math.sqrt(self.rec_dict['VCV_ZZ']),
-            # )
-            # print(output)
 
+            # Need to change this cos max output rate of INS_RMS is lower
+            if all(ins_records in self.records for ins_records in [INS_FULL_NAV, INS_RMS]):
+                # print "Full INS info, filling ROS messages"
+                self.send_ins_fix()
+                self.send_ins_attitude()
+            elif INS_FULL_NAV in self.records and rospy.get_time() - self.ins_rms_ts.to_sec() < self.ins_rms_timeout:
+                self.send_ins_fix()
+                self.send_ins_attitude()
+
+
+    def send_ins_fix(self):
+        current_time = rospy.get_rostime() # Replace with GPS time?
+        fix = NavSatFix()
+
+        fix.header.stamp = current_time
+        fix.header.frame_id = self.gps_main_frame_id
+
+        gps_qual = self.gps_qualities[self.rec_dict['GPS_QUALITY']]
+        fix.status.service = NavSatStatus.SERVICE_GPS # TODO: Fill correctly
+        fix.status.status = gps_qual[0]
+        fix.position_covariance_type = gps_qual[1]
+
+        fix.latitude = self.rec_dict['LATITUDE_INS']
+        fix.longitude = self.rec_dict['LONGITUDE_INS']
+        fix.altitude = self.rec_dict['ALTITUDE_INS']
+
+        fix.position_covariance[0] = self.rec_dict['RMS_LATITUDE'] ** 2
+        fix.position_covariance[4] = self.rec_dict['RMS_LONGITUDE'] ** 2
+        fix.position_covariance[8] = self.rec_dict['RMS_ALTITUDE'] ** 2
+
+        self.fix_pub.publish(fix)
+
+
+    def send_ins_attitude(self):
+        """
+        We send the GNSS fused attitude information as a PoseWithCovarianceStamped
+        msg to avoid confusion that it might come purely from an IMU
+        """
+        current_time = rospy.get_rostime() # Replace with GPS time?
+        self.ins_rms_ts = rospy.Time.now()
+
+        # print "current time: ", current_time, "INS time: ", self.ins_rms_ts
+        attitude = PoseWithCovarianceStamped()
+
+        attitude.header.stamp = current_time
+        attitude.header.frame_id = self.base_frame  # Assume transformation handled by receiver
+
+        heading_enu = 360 - self.normalize_angle(self.rec_dict['YAW'] + 270)
+        quaternion = quaternion_from_euler(math.radians(self.rec_dict['ROLL']),     #  roll sign stays the same
+                                           - math.radians(self.rec_dict['PITCH']),  # -ve for robots coord system (+ve down)
+                                           math.radians(heading_enu))
+        print 'r p y receiver_heading [degs]: ', self.rec_dict['ROLL'], self.rec_dict['PITCH'], heading_enu, self.rec_dict['YAW']
+        attitude.pose.pose.orientation.x = quaternion[0]
+        attitude.pose.pose.orientation.y = quaternion[1]
+        attitude.pose.pose.orientation.z = quaternion[2]
+        attitude.pose.pose.orientation.w = quaternion[3]
+
+        attitude.pose.covariance[23] = math.radians(self.rec_dict['RMS_ROLL']) ** 2  # [36] size array
+        attitude.pose.covariance[29] = math.radians(self.rec_dict['RMS_PITCH']) ** 2  # [36] size array
+        attitude.pose.covariance[35] = math.radians(self.rec_dict['RMS_YAW']) ** 2 # [36] size array
+
+        self.attitude_pub.publish(attitude)
+
+
+    @staticmethod
+    def normalize_angle(angle_in):
+        while angle_in > 360:
+            angle_in = angle_in - 360
+        while angle_in < 0:
+            angle_in = angle_in + 360
+        return angle_in
 
 
     def setup_connection(self, _ip, _port):
@@ -112,7 +236,7 @@ class GsofDriver(object):
         msg_field_names = ('STX', 'STATUS', 'TYPE', 'LENGTH',
                            'T_NUM', 'PAGE_INDEX', 'MAX_PAGE_INDEX')
         self.msg_dict = dict(zip(msg_field_names, unpack('>7B', data)))
-        print "msg dict: ", self.msg_dict
+        # print "msg dict: ", self.msg_dict
         self.msg_bytes = self.client.recv(self.msg_dict['LENGTH'] - 3)
         (checksum, etx) = unpack('>2B', self.client.recv(2))
 
@@ -124,137 +248,21 @@ class GsofDriver(object):
         else:
             self.checksum = False
 
-    def get_records(self):
-        while len(self.msg_bytes) > 0:
-            # READ THE FIRST TWO BYTES FROM RECORD HEADER
-            record_type, record_length = unpack('>2B', self.msg_bytes[0:2])
-            print "Record type: ", record_type
-            self.msg_bytes = self.msg_bytes[2:]
-            self.select_record(record_type, record_length)
 
-    def select_record(self, record_type, record_length):
-        """
-            Note: Capital means unsigned
-            B - Char
-            L - long
-            H - Short
-            d - Double
-            i - Int
-            f - Float
-            # - Byte c?
-        """
-        if record_type == 1:
-            rec_field_names = ('GPS_TIME', 'GPS_WEEK', 'SVN_NUM',
-                               'FLAG_1', 'FLAG_2', 'INIT_NUM')
-            rec_values = unpack('>LH4B', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 2:
-            rec_field_names = ('LATITUDE', 'LONGITUDE', 'HEIGHT')
-            rec_values = unpack('>3d', self.msg_bytes[0:record_length])
-            rec_values = list(rec_values)
-            rec_values = (math.degrees(rec_values[0]), math.degrees(rec_values[1]), rec_values[2])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-            print "rec dict: ", self.rec_dict
-        elif record_type == 3:
-            rec_field_names = ('X_POS', 'Y_POS', 'Z_POS')
-            rec_values = unpack('>3d', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 4:
-            rec_field_names = ('LOCAL_DATUM_ID', 'LOCAL_DATUM_LAT',
-                               'LOCAL_DATUM_LON', 'LOCAL_DATUM_HEIGHT', 'OPRT')
-            rec_values = unpack('>8s3dB', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 5:
-            rec_field_names = ('LOCAL_DATUM_ID', 'LOCAL_ZONE_ID',
-                               'LOCAL_ZONE_NORTH', 'LOCAL_ZONE_EAST', 'LOCAL_DATUM_HEIGHT')
-            rec_values = unpack('>2s3d', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 6:
-            rec_field_names = ('DELTA_X', 'DELTA_Y', 'DELTA_Z')
-            rec_values = unpack('>3d', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 7:
-            rec_field_names = ('DELTA_EAST', 'DELTA_NORTH', 'DELTA_UP')
-            rec_values = unpack('>3d', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 8:
-            rec_field_names = ('VEL_FLAG', 'VELOCITY', 'HEADING', 'VERT_VELOCITY')
-            rec_values = unpack('>B3f', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 9:
-            rec_field_names = ('PDOP', 'HDOP', 'VDOP', 'TDOP')
-            rec_values = unpack('>4f', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 10:
-            rec_field_names = ('CLOCK_FLAG', 'CLOCK_OFFSET', 'FREQ_OFFSET')
-            rec_values = unpack('>B2d', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 11:
-            rec_field_names = ('POSITION_RMS_VCV', 'VCV_XX', 'VCV_XY', 'VCV_XZ',
-                               'VCV_YY', 'VCV_YZ', 'VCV_ZZ', 'UNIT_VAR_VCV', 'NUM_EPOCHS_VCV')
-            rec_values = unpack('>8fh', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 12:
-            rec_field_names = ('POSITION_RMS_SIG', 'SIG_EAST', 'SIG_NORT', 'COVAR_EN', 'SIG_UP',
-                               'SEMI_MAJOR', 'SEMI_MINOR', 'ORIENTATION', 'UNIT_VAR_SIG',
-                               'NUM_EPOCHS_SIG')
-            rec_values = unpack('>9fh', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 15:
-            rec_field_names = 'SERIAL_NUM'
-            rec_values = unpack('>l', self.msg_bytes[0:record_length])
-            self.rec_dict.update({rec_field_names: rec_values[0]})
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 16:
-            rec_field_names = ('GPS_MS_OF_WEEK', 'CT_GPS_WEEK', 'UTC_OFFSET', 'CT_FLAGS')
-            rec_values = unpack('>l2hB', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 26:
-            rec_field_names = ('UTC_MS_OF_WEEK', 'UTC_GPS_WEEK', 'UTC_SVS_NUM', 'UTC_FLAG_1', 'UTC_FLAG_2',
-                               'UTC_INIT_NUM')
-            rec_values = unpack('>lh4B', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 34:
-            NUM_OF_SVS = unpack('>B', self.msg_bytes[0])
-            self.msg_bytes = self.msg_bytes[1:]
-            rec_field_names = ('PRN', 'SV_SYSTEM', 'SV_FLAG1', 'SV_FLAG2', 'ELEVATION', 'AZIMUTH',
-                               'SNR_L1', 'SNR_L2', 'SNR_L5')
-            for field in xrange(len(rec_field_names)):
-                self.rec_dict[rec_field_names[field]] = []
-            for sat in xrange(NUM_OF_SVS[0]):
-                rec_values = unpack('>5Bh3B', self.msg_bytes[0:10])
-                self.msg_bytes = self.msg_bytes[10:]
-                for num in xrange(len(rec_field_names)):
-                    self.rec_dict[rec_field_names[num]].append(rec_values[num])
-        elif record_type == 37:
-            rec_field_names = ('BATT_CAPACITY', 'REMAINING_MEM')
-            rec_values = unpack('>hd', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        elif record_type == 41:
-            rec_field_names = ('GPS_TIME_REF', 'GPS_WEEK_REF', 'LAT_REF', 'LONG_REF',
-                               'HEIGHT_REF', "QI")
-            rec_values = unpack('>LH3dB', self.msg_bytes[0:record_length])
-            self.rec_dict.update(dict(zip(rec_field_names, rec_values)))
-            self.msg_bytes = self.msg_bytes[record_length:]
-        else:
-            """Unknown record type? Skip it for now!"""
-            #print record_type
-            self.msg_bytes = self.msg_bytes[record_length:]
+    def get_records(self):
+        self.byte_position = 0
+        while self.byte_position < len(self.msg_bytes):
+            # READ THE FIRST TWO BYTES FROM RECORD HEADER
+            record_type, record_length = unpack('>2B', self.msg_bytes[self.byte_position:self.byte_position + 2])
+            self.byte_position += 2
+            self.records.append(record_type)
+            # print "Record type: ", record_type, " Length: ", record_length
+            # self.select_record(record_type, record_length)
+            if record_type in parse_maps:
+                self.rec_dict.update(dict(zip(parse_maps[record_type][0], unpack(parse_maps[record_type][1], self.msg_bytes[self.byte_position:self.byte_position + record_length]))))
+            else:
+                rospy.logwarn("Record type %s is not in parse maps.", record_type)
+            self.byte_position += record_length
 
 
 
--- src/trimble_gnss_driver/parser.py
+++ src/trimble_gnss_driver/parser.py
@@ -7,3 +7,207 @@ GSOF parser
 """
 
 
+parse_maps = {
+    1: [
+        ('GPS_TIME',
+         'GPS_WEEK',
+         'SVN_NUM',
+         'FLAG_1',
+         'FLAG_2',
+         'INIT_NUM'),
+        ('>LH4B')
+       ],
+    2: [
+        ('LATITUDE',
+         'LONGITUDE',
+         'HEIGHT'),
+        ('>3d'),
+       ],
+    3: [
+        ('X_POS',
+         'Y_POS',
+         'Z_POS'),
+        ('>3d')
+       ],
+    4: [
+        ('LOCAL_DATUM_ID',
+         'LOCAL_DATUM_LAT',
+         'LOCAL_DATUM_LON',
+         'LOCAL_DATUM_HEIGHT',
+         'OPRT'),
+        ('>8s3dB')
+       ],
+    5: [
+        ('LOCAL_DATUM_ID',
+         'LOCAL_ZONE_ID',
+         'LOCAL_ZONE_NORTH',
+         'LOCAL_ZONE_EAST',
+         'LOCAL_DATUM_HEIGHT'),
+        ('>2s3d')
+       ],
+    6: [
+        ('DELTA_X',
+         'DELTA_Y',
+         'DELTA_Z'),
+        ('>3d')
+       ],
+    7: [
+        ('DELTA_EAST',
+         'DELTA_NORTH',
+         'DELTA_UP'),
+        ('>3d')
+       ],
+    8: [
+        ('VEL_FLAG',
+         'VELOCITY',
+         'HEADING',
+         'VERT_VELOCITY'),
+        ('>B3f')
+       ],
+    9: [
+        ('PDOP',
+         'HDOP',
+         'VDOP',
+         'TDOP'),
+        ('>4f')
+       ],
+    10:[
+        ('CLOCK_FLAG',
+         'CLOCK_OFFSET',
+         'FREQ_OFFSET'),
+        ('>B2d')
+       ],
+    11:[
+        ('POSITION_RMS_VCV',
+         'VCV_XX',
+         'VCV_XY',
+         'VCV_XZ',
+         'VCV_YY',
+         'VCV_YZ',
+         'VCV_ZZ',
+         'UNIT_VAR_VCV',
+         'NUM_EPOCHS_VCV'),
+        ('>8fh')
+       ],
+    12:[
+        ('POSITION_RMS_SIG',
+         'SIG_EAST',
+         'SIG_NORT',
+         'COVAR_EN',
+         'SIG_UP',
+         'SEMI_MAJOR',
+         'SEMI_MINOR',
+         'ORIENTATION',
+         'UNIT_VAR_SIG',
+         'NUM_EPOCHS_SIG'),
+        ('>9fh')
+       ],
+    15:[
+        ('SERIAL_NUM'),
+        ('>l')
+       ],
+    16:[
+        ('GPS_MS_OF_WEEK',
+         'CT_GPS_WEEK',
+         'UTC_OFFSET',
+         'CT_FLAGS'),
+        ('>l2hB')
+       ],
+    26:[
+        ('UTC_MS_OF_WEEK',
+         'UTC_GPS_WEEK',
+         'UTC_SVS_NUM',
+         'UTC_FLAG_1',
+         'UTC_FLAG_2',
+         'UTC_INIT_NUM'),
+        ('>lh4B')
+       ],
+    27:[
+        ('GPS_TIME',
+        'FLAGS',
+        'SVN_NUM',
+        'CALC_MODE',
+        'RESERVED',
+        'PITCH',
+        'YAW',
+        'ROLL',
+        'MASTER_SLAVE_RANGE',
+        'PDOP',
+        'PITCH_VAR',
+        'YAW_VAR',
+        'ROLL_VAR',
+        'PITCH_YAW_COVAR',
+        'PITCH_ROLL_COVAR',
+        'YAW_ROLL_COVAR',
+        'MASTER_SLAVE_RANGE_VAR'),
+        ('>L4B4dH7f')
+       ],
+    # 34 needs to be dealth with carefully/differently as it depends on the number of
+    # satellites in view.
+    # 34:[
+    #     ('PRN',
+    #      'SV_SYSTEM',
+    #      'SV_FLAG1',
+    #      'SV_FLAG2',
+    #      'ELEVATION',
+    #      'AZIMUTH',
+    #      'SNR_L1',
+    #      'SNR_L2',
+    #      'SNR_L5'),
+    #     ('>5Bh3B')
+    #    ],
+    37:[
+        ('BATT_CAPACITY',
+         'REMAINING_MEM'),
+        ('>hd')
+       ],
+    41:[
+        ('GPS_TIME_REF',
+         'GPS_WEEK_REF',
+         'LAT_REF',
+         'LONG_REF',
+         'HEIGHT_REF',
+          'QI'),
+        ('>LH3dB')
+       ],
+    49:[
+        ('GPS_WEEK',
+         'GPS_TIME',
+         'IMU_ALIGNMENT_STATUS',
+         'GPS_QUALITY',
+         'LATITUDE_INS',
+         'LONGITUDE_INS',
+         'ALTITUDE_INS',
+         'VEL_NORTH',
+         'VEL_EAST',
+         'VEL_DOWN',
+         'TOTAL_SPEED',
+         'ROLL',
+         'PITCH',
+         'YAW',
+         'TRACK_ANGLE',
+         'VEL_ROLL',
+         'VEL_PITCH',
+         'VEL_YAW',
+         'ACC_X',
+         'ACC_Y',
+         'ACC_Z'),
+        ('>HL2B3d4f4d6f')
+       ],
+    50:[
+        ('GPS_WEEK',
+         'GPS_TIME',
+         'IMU_ALIGNMENT_STATUS',
+         'GPS_QUALITY',
+         'RMS_LATITUDE',
+         'RMS_LONGITUDE',
+         'RMS_ALTITUDE',
+         'RMS_VEL_NORTH',
+         'RMS_VEL_EAST',
+         'RMS_VEL_DOWN',
+         'RMS_ROLL',
+         'RMS_PITCH',
+         'RMS_YAW'),
+        ('>HL2B9f')
+       ]
+    }
